be56c6225e3219e742d048899a944d91e8ff8360
Leonardo Barreto <lapb@icomp.ufam.edu.br>
2021-03-20 18:18:58 -0400

    fix: change from HelloWorld to Home component
    Signed-off-by: Leonardo Barreto <lapb@icomp.ufam.edu.br>

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ this.title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: "Manaus Bordados",
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  margin: auto;
+}
+</style>
